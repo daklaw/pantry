95cc0fd1364f2bb1329a208b22071464d20fc0fc
Phil Wee <cyber_peewee@yahoo.com>
2014-03-03 02:26:47 -0800

    Debug problem with swipe functionality

--- pantry/RecipeDetailSwipeViewController.m
+++ pantry/RecipeDetailSwipeViewController.m
@@ -80,6 +80,15 @@
         self.ingredientsView.separatorStyle = UITableViewCellSeparatorStyleNone;
     }
     
+    //set background color
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    view.backgroundColor = [UIColor colorWithRed:red
+                                           green:green
+                                            blue:blue
+                                           alpha:1.0];
+    
     // Populate view with recipe details
     Recipe *recipe = self.recipes[index];
     self.nameLabel.text = recipe.name;
